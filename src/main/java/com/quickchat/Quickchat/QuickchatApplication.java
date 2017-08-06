3c601eb1b28486ad5c9333fa8400353d3d3c5347
Chetan <chetankhatale24@gmail.com>
2017-08-07 02:02:27 +0530

    Test Commits

--- src/main/java/com/quickchat/Quickchat/QuickchatApplication.java
+++ src/main/java/com/quickchat/Quickchat/QuickchatApplication.java
@@ -9,5 +9,6 @@ public class QuickchatApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(QuickchatApplication.class, args);
 		System.out.println("Hello");
+		System.out.println("Hi");
 	}
 }
