bf077e06c44697275a3e5da17f19cc1ab0840f06
Chetan <chetankhatale24@gmail.com>
2017-08-07 01:08:31 +0530

    Initial Commits

---
+++ src/main/java/com/quickchat/Quickchat/QuickchatApplication.java
@@ -0,0 +1,12 @@
+package com.quickchat.Quickchat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuickchatApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QuickchatApplication.class, args);
+	}
+}
---
+++ src/test/java/com/quickchat/Quickchat/QuickchatApplicationTests.java
@@ -0,0 +1,16 @@
+package com.quickchat.Quickchat;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class QuickchatApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
