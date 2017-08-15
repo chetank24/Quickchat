415d6da13c1d17b971ab28bbcf560ff317e3a238
Chetan <chetankhatale24@gmail.com>
2017-08-15 23:12:08 +0530

    Code changes and improvement

--- src/main/java/com/quickchat/controller/ChatController.java
+++ src/main/java/com/quickchat/controller/ChatController.java
@@ -13,19 +13,17 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class ChatController {
-	
+
 	public static String jid = null;
 	public static Map<String, String> mapping = new HashMap<>();
 	public static Map<String, String> messages = new HashMap<>();
 
-
-	
-	
 	@RequestMapping(value = "/setm")
 	public ResponseEntity<String> msgPost(HttpSession httpSession, HttpServletRequest httpRequest) {
 
-		//if (mapping.get(httpSession.getId()) == null)
-			//setup(httpSession, httpRequest);//return ResponseEntity.status(HttpStatus.ACCEPTED).body(null);
+
+		// if (mapping.get(httpSession.getId()) == null)
+		// setup(httpSession, httpRequest);
 
 		String myMessages = httpRequest.getParameter("message");
 		messages.put(httpSession.getId(), myMessages);
@@ -36,20 +34,24 @@ public class ChatController {
 	@RequestMapping(value = "/getm")
 	public ResponseEntity<String> msgGet(HttpSession httpSession, HttpServletRequest httpRequest) {
 
-		//if (mapping.get(httpSession.getId()) == null)
-			//setup(httpSession, httpRequest);
-
+		// if (mapping.get(httpSession.getId()) == null)
+		// setup(httpSession, httpRequest);
+		
+		/*System.out.println("my ID ="+httpSession.getId() );
+		System.out.println("Partner ID ="+mapping.get(httpSession.getId()) );
+		System.out.println("My msg ="+messages.get(httpSession.getId()) );
+		System.out.println("Partner msg ="+messages.get(mapping.get(httpSession.getId())));*/
 		String partnerMsgs = (String) messages.get(mapping.get(httpSession.getId()));
 		return ResponseEntity.status(HttpStatus.OK).body(partnerMsgs);
 	}
-	
-	
-	
+
 	@RequestMapping("/setup")
-	public ResponseEntity<String> setup( HttpSession httpSession,HttpServletRequest httpRequest) {
-		if (jid == null && !mapping.containsKey(httpSession.getId()) ) {
+	public synchronized ResponseEntity<String> setup(HttpSession httpSession, HttpServletRequest httpRequest) {
+		//System.out.println(httpSession.getId() + "  in setup");
+		
+		if (jid == null && !mapping.containsKey(httpSession.getId())) {
 			jid = httpSession.getId();
-			System.out.println(httpSession.getId()+"  is waiting");
+			//System.out.println(httpSession.getId() + "  is waiting");
 			return ResponseEntity.status(HttpStatus.OK).body("Waiting");
 		}
 		if (jid != null && !jid.equals(httpSession.getId())) {
@@ -57,23 +59,54 @@ public class ChatController {
 			mapping.put(jid, httpSession.getId());
 			jid = null;
 		}
-		if(jid != null && mapping.get(jid)==null )
-		{
-			System.out.println(httpSession.getId()+"  is waiting");
+		if (jid != null && mapping.get(jid) == null) {
+			//System.out.println(httpSession.getId() + "  is waiting");
 			return ResponseEntity.status(HttpStatus.OK).body("Waiting");
 		}
-		/*if(mapping.get(httpSession.getId())!= null && context.getAttribute(mapping.get(httpSession.getId()))==null)
-		{
-			return ResponseEntity.status(HttpStatus.ACCEPTED).body(null);
-		}*/
+		
+		
+
 		return ResponseEntity.status(HttpStatus.OK).body("Connected");
 	}
 	
-	@RequestMapping("/clean")
-	public ResponseEntity<String> clean( HttpSession httpSession,HttpServletRequest httpRequest) {
+	@RequestMapping("/verifyConnection")
+	public synchronized ResponseEntity<String> verifyConnection(HttpSession httpSession, HttpServletRequest httpRequest) {
+		//System.out.println(httpSession.getId() + "  in verifyConnection");
+				if(httpSession.getId().equals(mapping.get(mapping.get(httpSession.getId()))))
+		{
+			return ResponseEntity.status(HttpStatus.OK).body("Connected");
+		}
+		if(httpSession.getId().equals(jid))
+		{
+			return ResponseEntity.status(HttpStatus.OK).body("Waiting");
+		}
+		
+		return ResponseEntity.status(HttpStatus.OK).body("Disconnected");
+	}
+
+	@RequestMapping("/reset")
+	public ResponseEntity<String> clean(HttpSession httpSession, HttpServletRequest httpRequest) {
 		mapping = new HashMap<>();
 		messages = new HashMap<>();
 		return ResponseEntity.status(HttpStatus.OK).body("Cleaned");
 	}
-	
+
+	@RequestMapping("/cleanUp")
+	public synchronized ResponseEntity<String> cleanUp(HttpSession httpSession, HttpServletRequest httpRequest) {
+
+		if (messages.get(mapping.get(httpSession.getId())) != null)
+			messages.remove(mapping.get(httpSession.getId()));
+
+		if (messages.get(httpSession.getId()) != null)
+			messages.remove(httpSession.getId());
+
+		if (mapping.get(mapping.get(httpSession.getId())) != null)
+			mapping.remove(mapping.get(httpSession.getId()));
+
+		if (mapping.get(httpSession.getId()) != null)
+			mapping.remove(httpSession.getId());
+
+		return ResponseEntity.status(HttpStatus.OK).body("Cleaned");
+	}
+
 }
--- src/main/resources/static/communicate.js
+++ src/main/resources/static/communicate.js
@@ -2,63 +2,60 @@
  * 
  */
 
-var tempmsg="";
-var myMsg="";
-
-
+var tempmsg = "";
+var connectionStatus = "Connecting...";
 
 function getMessage() {
-	
-//	alert("getting messages");
-	var xhttp = new XMLHttpRequest();
-	  xhttp.onreadystatechange = function() {
-	    if (this.readyState == 4 && this.status == 200) {
-	     
-	    	if(tempmsg!=this.responseText)
-	    		{
-	    		//document.getElementById("you").innerHTML=this.responseText;
-	    		var para = document.createElement("P");
-	    		var t = document.createTextNode(this.responseText);
-	    		para.appendChild(t); 
-	    		document.getElementById("you").appendChild(para);
-	    		tempmsg=this.responseText;
-	    		
-	    		var objDiv = document.getElementById("you");
-	    		objDiv.scrollTop = objDiv.scrollHeight;
-	    		}
-	    		
-	    	
-	    	 
-	    }
-	  };
-	  xhttp.open("GET", "/getm", true);
-	  xhttp.send();
-}
 
+	//	alert("getting messages");
+	//alert(connectionStatus);
+	if (connectionStatus == "Connected") {
+		var xhttp = new XMLHttpRequest();
+		xhttp.onreadystatechange = function() {
+			if (this.readyState == 4 && this.status == 200) {
+
+				if (tempmsg != this.responseText) {
+					//document.getElementById("you").innerHTML=this.responseText;
+					var para = document.createElement("P");
+					var t = document.createTextNode("Alien : "+this.responseText);
+					para.appendChild(t);
+					document.getElementById("you").appendChild(para);
+					tempmsg = this.responseText;
+
+					var objDiv = document.getElementById("you");
+					objDiv.scrollTop = objDiv.scrollHeight;
+				}
+
+			}
+		};
+		xhttp.open("GET", "/getm", true);
+		xhttp.send();
+
+	}
+}
 
 function sendMessage() {
-	var params = "message="+document.getElementById("messageInput").value;
+	var params = "message=" + document.getElementById("messageInput").value;
 	var xhttp = new XMLHttpRequest();
-	  xhttp.onreadystatechange = function() {
-	    if (this.readyState == 4 && this.status == 200) {
-	        	
-	    	var para = document.createElement("P");
-	    	//para.style.bottom=0px;
-    		var t = document.createTextNode(document.getElementById("messageInput").value);
-    		para.appendChild(t); 
-    		document.getElementById("you").appendChild(para);
-	    	
-	    	document.getElementById("messageInput").value="";
-	    	
-	    	var objDiv = document.getElementById("you");
-    		objDiv.scrollTop = objDiv.scrollHeight;
-	    }
-	  };
-	  xhttp.open("POST", "/setm?"+params, true);
-	  xhttp.send(params);
-}
+	xhttp.onreadystatechange = function() {
+		if (this.readyState == 4 && this.status == 200) {
 
-var connectionStatus="Connecting";
+			var para = document.createElement("P");
+			//para.style.bottom=0px;
+			var t = document.createTextNode("Me    : "+document
+					.getElementById("messageInput").value);
+			para.appendChild(t);
+			document.getElementById("you").appendChild(para);
+
+			document.getElementById("messageInput").value = "";
+
+			var objDiv = document.getElementById("you");
+			objDiv.scrollTop = objDiv.scrollHeight;
+		}
+	};
+	xhttp.open("POST", "/setm?" + params, true);
+	xhttp.send(params);
+}
 
 function setup() {
 	document.getElementById("status").innerHTML = connectionStatus;
@@ -73,14 +70,52 @@ function setup() {
 		xhttp.open("GET", "/setup", true);
 		xhttp.send();
 	}
+	
 }
 
-setInterval(function(){ getMessage(); }, 4000);
+function cleanUp() {
+	document.getElementById("status").innerHTML = "Cleaning Up..";
+
+	var xhttp = new XMLHttpRequest();
+	xhttp.onreadystatechange = function() {
+		if (this.readyState == 4 && this.status == 200) {
+			document.getElementById("status").innerHTML = this.responseText;
+			connectionStatus = this.responseText;
+		}
+	};
+	xhttp.open("GET", "/cleanUp", true);
+	xhttp.send();
+
+}
+
+
+function verifyConnection() {
+	
+		var xhttp = new XMLHttpRequest();
+		xhttp.onreadystatechange = function() {
+			if (this.readyState == 4 && this.status == 200) {
+				document.getElementById("status").innerHTML = this.responseText;
+				connectionStatus = this.responseText;
+			}
+		};
+		xhttp.open("GET", "/verifyConnection", true);
+		xhttp.send();
+	
+	
+}
+setInterval(function() {
+	getMessage();
+}, 2000);
+
+setInterval(function() {
+	verifyConnection();
+}, 3000);
+
 
 
 function runScript(e) {
-    if (e.keyCode == 13) {
-    	sendMessage();
-        return false;
-    }
+	if (e.keyCode == 13) {
+		sendMessage();
+		return false;
+	}
 }
