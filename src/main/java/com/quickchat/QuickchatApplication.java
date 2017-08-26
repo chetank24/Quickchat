d9aaf84ef3d45f523c1ea0620ac7e5022db5b6c5
Chetan <chetankhatale24@gmail.com>
2017-08-26 13:06:29 +0530

    added ureka service discovery

--- src/main/java/com/quickchat/QuickchatApplication.java
+++ src/main/java/com/quickchat/QuickchatApplication.java
@@ -2,11 +2,23 @@ package com.quickchat;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+//extend SpringBootServletInitializer and override configure method
+//if you want to deploy war on servlet container which is not embedded
 
 @SpringBootApplication
-public class QuickchatApplication {
+@EnableDiscoveryClient
+public class QuickchatApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(QuickchatApplication.class, args);
 	}
+	
+	 @Override
+	    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+	        return application.sources(QuickchatApplication.class);
+	    }
 }
